fb4ac47a93e38b1e94abdb8e53ba1fcafbbed3d2
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-30 15:47:34 -0400

    Completed exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace BankEncapsulation
+﻿using System.Diagnostics;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myAccount = new BankAccount();
+            
+            Console.WriteLine("Type Deposit if you would like to Deposit, Withdraw if you would like to remove balance, or Get if you would like to retrieve your current balance.");
+            var userInput = Console.ReadLine();
+            switch(userInput)
+            {
+                case "Deposit": 
+                    Console.WriteLine($"Alright, how much would you like to deposit?");
+
+                    double number = 0;
+                    
+                    if (double.TryParse(Console.ReadLine(), out number))
+                    {
+                        Console.WriteLine($"You've deposited money successfully!");
+                        myAccount.Deposit(number);
+                        Console.WriteLine(myAccount.GetBalance());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                    break;
+                case "Withdraw":
+                    if (double.TryParse(Console.ReadLine(), out number))
+                    {
+                        Console.WriteLine($"You've deposited money successfully!");
+                        myAccount.Withdraw(number);
+                        Console.WriteLine(myAccount.GetBalance());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+
+                    break;
+                case "Get":
+                    Console.WriteLine(myAccount.GetBalance());
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
         }
     }
 }
